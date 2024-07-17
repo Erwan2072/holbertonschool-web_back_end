52a614e6de8760281c62b06946c47effe93657cd
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-07-17 11:02:50 +0200

    modif

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,47 +1,18 @@
-import Currency from './3-currency.js';
-
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
-  // Getter for amount
-  get amount() {
-    return this._amount;
-  }
+  get amount() { return this._amount; }
 
-  // Setter for amount
-  set amount(newAmount) {
-    if (typeof newAmount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    this._amount = newAmount;
-  }
+  set amount(value) { this._amount = value; }
 
-  // Getter for currency
-  get currency() {
-    return this._currency;
-  }
+  get currency() { return this._currency; }
 
-  // Setter for currency
-  set currency(newCurrency) {
-    if (!(newCurrency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of Currency');
-    }
-    this._currency = newCurrency;
-  }
+  set currency(value) { this._currency = value; }
 
-  // Method to display full price
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-  }
+  displayFullPrice() { return `${this._amount} ${this._currency.name} (${this._currency.code})`; }
 
-  // Static method to convert price
-  static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
-      throw new TypeError('Both amount and conversionRate must be numbers');
-    }
-    return amount * conversionRate;
-  }
+  static convertPrice(amount, conversionRate) { return amount * conversionRate; }
 }
