432201c97bd00876a324aafbfedd15834a7c5e6d
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-07-22 14:41:08 +0200

    correction task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,18 +1,7 @@
 function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') return '';
-  const cleanedValues = [];
 
-  if (startString) {
-    for (const value of set) {
-      if (value.startsWith(startString)) {
-
-        cleanedValues.push(value.slice(startString.length));
-      }
-    }
-
-    return cleanedValues.join('-');
-  }
-  return '';
-}
-
-export default cleanSet;
+  return Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
