649074d392c2057f2c23375ed5fed7847dfb8e84
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-07-23 09:58:27 +0200

    correction task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-  }).catch(() => {
-    console.log('Got a response from the API');
-  });
-}
+  promise.then(() => ({ stattus: 200, body: 'success' }))
+  promise.catch(() => Error())
+  promise.finally (() => console.log('Got a response from the API'));
+  };
