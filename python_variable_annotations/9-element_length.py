39a6e66a3db4d667051e7808317d0710556cdcbe
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-08-14 09:51:47 +0200

    correction pycodestyle

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -7,4 +7,3 @@ from typing import Sequence, Tuple, List, Dict, Any
 def element_length(lst: Sequence[Sequence]) -> List[Tuple[Sequence, int]]:
     """element length function"""
     return [(i, len(i)) for i in lst]
-
