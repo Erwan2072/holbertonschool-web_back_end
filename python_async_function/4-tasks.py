7a1a4da416624aaabe6bf4ad87b2a2e12e8b93d5
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-08-16 11:08:57 +0200

    refonte de la task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
-"""
-Alter wait_n to use tasks.
+""" This module contains an asynchronous function that schedules multiple delay tasks
+and returns a list of these delays in ascending order.
 """
 import asyncio
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
-task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
-    """Run task_wait_random n times and return the list of delays."""
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    return sorted(delays)
+task_wait_random = __import__('3-tasks').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """This function creates each task sequentially and awaits its completion before moving to the next,
+    collecting all the results into a list which is then returned sorted.
+    """
+    list_of_delay = []
+    for turn in range(n):
+        list_of_delay.append(
+            await asyncio.create_task(task_wait_random(max_delay)))
+    return sorted(list_of_delay)
+task_wait_n
