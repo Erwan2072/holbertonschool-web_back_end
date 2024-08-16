fc0aa5ddd7f9d2ec28e17daabe96b9c2a2de877f
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-08-16 10:54:46 +0200

    correction output 2

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -3,11 +3,12 @@
 Alter wait_n to use tasks.
 """
 import asyncio
-
+wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> list:
     """Run task_wait_random n times and return the list of delays."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
