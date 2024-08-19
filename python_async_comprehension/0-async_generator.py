53eb17c08ea571afe462b0e7c269e6b92a9d12b2
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-08-19 10:24:11 +0200

    add import typing

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+import typing
 
 
-async def async_generator():
+async def async_generator() -> typing.Generator[float, None, None]:
     """ Generate a random number every second """
     for _ in range(10):
         await asyncio.sleep(1)
