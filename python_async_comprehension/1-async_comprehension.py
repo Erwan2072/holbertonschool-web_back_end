18ce206f04ff44638788fe3194b54b3cd089bcea
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-08-19 10:54:14 +0200

    correction task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
+
 """ Let's execute multiple coroutines at the same time with async """
 
-import asyncio
 import typing
-wait_random = __import__('0-async_generator').wait_random
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> typing.List[float]:
-    """
-    Collect 10 random numbers using an async
-    comprehensing over async_generator
-    """
-    return [i async for i in wait_random()]
+    """ Collect 10 random numbers using an async comprehensing over async_generator """
+    random_numbers = [number async for number in async_generator()]
+    return random_numbers[:10]
+
