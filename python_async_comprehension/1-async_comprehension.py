22273614d9a2ee033ae1eaa73a943cab38619650
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-08-19 10:22:01 +0200

    pycodestyles

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,5 +6,8 @@ wait_random = __import__('0-async_generator').wait_random
 
 
 async def async_comprehension() -> list[float]:
-    """ Collect 10 random numbers using an async comprehensing over async_generator """
+    """
+    Collect 10 random numbers using an async
+    comprehensing over async_generator
+    """
     return [i async for i in wait_random()]
