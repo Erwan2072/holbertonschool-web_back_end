89586f748758c3a7dad4ded456629884ac1d72e0
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-08-19 11:00:15 +0200

    push task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,12 +3,12 @@
 """ Let's execute multiple coroutines at the same time with async """
 
 import asyncio
-wait_random = __import__('1-async_comprehension').wait_random
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """ Measure the runtime of async_comprehension """
     start = asyncio.get_event_loop().time()
-    await asyncio.gather(*(wait_random() for _ in range(4)))
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
     end = asyncio.get_event_loop().time()
     return end - start
