cdceb6955ea4d6838486241dd85f2d245fbb6f9f
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-09-18 09:56:39 +0200

    correction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,12 @@
+// 1-stdin.js
+console.log("Welcome to Holberton School, what is your name?");
 
-console.log("Welcome to Holberton School, what is your name?")
-// active stdin
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-
-// lisen enter from user
-process.stdin.on('data', function(data) {
-  const name = data.trim();
-  console.log(`Your name is: ${name}` );
-
-
-  console.log('This important software is now closing');
+process.stdin.on('data', (data) => {
+  const inputName = data.toString().trim(); // Pour enlever les espaces ou sauts de ligne
+  console.log(`Your name is: ${inputName}`);
   process.exit();
+});
 
+process.on('exit', () => {
+  console.log("This important software is now closing");
 });
