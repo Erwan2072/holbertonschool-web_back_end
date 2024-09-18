7f268dcf8cc78bcd2fcc30f6de54d378f0d46253
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-09-18 10:32:33 +0200

    modification task 1 to 7

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,17 +1,14 @@
 const express = require('express');
 
-// Créer l'application Express
 const app = express();
+const PORT = 1245;
 
-// Définir la route pour "/"
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 
-// Le serveur écoute sur le port 1245
-app.listen(1245, () => {
-  console.log('Server listening on port 1245');
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
-// Exporter l'application pour d'éventuels tests ou utilisation
 module.exports = app;
