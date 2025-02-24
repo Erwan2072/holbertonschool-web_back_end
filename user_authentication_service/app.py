24e3c5387fe3c5519acbd1a4847400d943aa1f38
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-02-24 11:44:27 +0100

    7. Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,9 +3,11 @@
 Basic Flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -14,6 +16,35 @@ def welcome():
     Simple GET route that returns a welcome message.
     """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """
+    Endpoint to register a new user.
+
+    Expected form data:
+    - email (str): The user's email
+    - password (str): The user's password
+
+    Returns:
+        JSON response with a success or error message.
+    """
+    # Récupération des données du formulaire
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # Vérification si les champs sont bien fournis
+    if not email or not password:
+        return jsonify({"message": "Missing email or password"}), 400
+
+    try:
+        # Tentative d'enregistrement du nouvel utilisateur
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        # Gestion du cas où l'utilisateur existe déjà
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
