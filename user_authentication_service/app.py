e4b111bc0ca9de1eceac2c40731ddc091d85cee3
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-02-24 11:36:13 +0100

    correction pycodestyle

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -7,12 +7,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def welcome():
     """
     Simple GET route that returns a welcome message.
     """
     return jsonify({"message": "Bienvenue"}), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
