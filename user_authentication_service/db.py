c8b261771bc8e4aab75f514df32f9e2a6612fc01
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-02-24 10:06:46 +0100

    correction pycodestyle

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -15,7 +15,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=False)
-        Base.metadata.drop_all(self._engine)  # Supprime et recrée la table à chaque exécution
+        Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
