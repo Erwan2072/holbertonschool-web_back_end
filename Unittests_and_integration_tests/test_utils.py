1ec9b25b5c68b8c7819b85a2d754f29b80d3c216
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-03-03 09:17:43 +0100

    correction pycodestyle

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -11,6 +12,7 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map returns the expected output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
