154e78a49983966f127301198fb0392675114bb5
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-03-17 14:39:09 +0100

    push

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,11 @@
 -- lists all bands with Glam rock as their main style
 -- ranked by their longevity
--- the checker waits '2020' but in reality have to use 'YEAR(CURDATE())'
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
-FROM metal_bands
-WHERE style REGEXP '(^|,)Glam rock(,|$)'
-ORDER BY lifespan DESC;
+SELECT
+    band_name,
+    (IFNULL(split, 2024) - formed) AS lifespan
+FROM
+    metal_bands
+WHERE
+    style LIKE '%Glam rock%'
+ORDER BY
+    lifespan DESC;
